9b1ae1abc9f4327098663da5dd573b2d44c30e73
Juliano Cesar da Silva <110416349+juli4n03@users.noreply.github.com>
2022-10-05 11:18:29 -0300

    atualizações de navegação entre páginas

---
+++ src/app/pages/login/Login.tsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+
+
+const history = useNavigate();
+
+const handleClick = () => {
+    history('/pagina-inicial')
+}
+    return(
+        <div>
+            Login
+            <button onClick={(handleClick)}>Página Inicial</button>
+        </div>
+    );
+}
+
+
