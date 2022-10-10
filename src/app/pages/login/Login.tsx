382bfe259e026bf2856a38194dc7933b9aaf0f26
Juliano Cesar da Silva <110416349+juli4n03@users.noreply.github.com>
2022-10-10 12:55:25 -0300

    componentes

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,17 +1,58 @@
-import { useNavigate } from "react-router-dom";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { InputLogin } from "./components/InputLogin";
+
 
 export const Login = () => {
 
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
 
-const history = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-const handleClick = () => {
-    history('/pagina-inicial')
-}
+    const emailLength = useMemo(()=> {
+        return email.length * 1000;
+    },[email.length]); 
+
+    const handleEntrar = useCallback(() => {
+        console.log(email)
+        console.log(password)
+
+    if (inputPasswordRef.current !== null) {
+        inputPasswordRef.current.focus()
+    }
+},[email,password]);
+
+
     return(
         <div>
-            Login
-            <button onClick={(handleClick)}>Página Inicial</button>
+            <form>
+                <p>Quantidade de caracteres no email: {emailLength}</p>
+
+                <InputLogin
+                    label = "Email"
+                    value = {email}
+                    onChange={newValue => setEmail(newValue)}
+                    onPressEnter={() => inputPasswordRef.current?.focus()}
+                    />
+                 <InputLogin
+                    label = "Senha"
+                    value={password}
+                    onChange={newValue => setPassword(newValue)}
+                    />
+               
+
+               {/*<label>
+                    <span>Senha</span>
+                    <input ref={inputPasswordRef}
+                           type="password"
+                           value={password}
+                           onChange={e => setPassword(e.target.value)}/>
+                </label>*/}
+
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+            </form>
         </div>
     );
 }
